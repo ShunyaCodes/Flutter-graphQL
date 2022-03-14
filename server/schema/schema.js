748f891f779d5a0d4cfbf4d79eb8f19ff3a68f14
ShunyaCodes <patilabhilash1046@gmail.com>
2022-03-13 19:48:39 -0800

    created RootQuery fetcjing all users and data related to them

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -124,6 +124,13 @@ const RootQuery = new GraphQLObjectType({
         
         },
 
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parent, args){
+                return usersData;
+            }
+        },
+
         hobby: {
             type: HobbyType,
             args: {id: {type:GraphQLID}},
