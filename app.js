2ef3c5ec58ffcdb245b3805e507876650536bc49
ShunyaCodes <patilabhilash1046@gmail.com>
2022-03-13 23:51:37 -0800

    Connected to mongoDB Database

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./server/schema/schema");
+const mongoose = require("mongoose");
+
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+
+
+app.use('/graphql', graphqlHTTP({
+        graphiql: true,
+        schema: schema,
+}));
+
+
+//connection with MongoDB Database
+mongoose.connect
+(`mongodb+srv://${process.env.mongoUserName}:${process.env.mongoUserPassword}@graphqlcluster.1g4wq.mongodb.net/${process.env.MongoDatabase}?retryWrites=true&w=majority`,
+{ useNewUrlParser: true, useUnifiedTopology: true}
+)
+.then(()=>{
+        app.listen({port:port}, ()=> { 
+        console.log(process.env.mongoUserName);
+        //localhost:4000
+        console.log('Listening for rquests on port' + port);
+    });
+}).catch((e) => {
+    console.log("Error:::" + e);
+});
+
